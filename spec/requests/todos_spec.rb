3c8294cf25cef719b48c09729089495bc7d7a5f8
David White <whited75@gmail.com>
2016-06-24 16:26:59 -0600

    Add RSpec test for Todo#show

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -5,14 +5,22 @@ RSpec.describe "Todos", type: :request do
 
   let(:completed_todo) { completed_todo = create(:completed_todo) }
 
-  let!(:todo_list) { todo_list = create_list(:todo, 10) }
+  let(:todo_list) { todo_list = create_list(:todo, 10) }
 
   describe "GET /todos" do
     it "sends a list of todos" do
+      todo_list
       get todos_path
-      json = JSON.parse(response.body)
       expect(response).to have_http_status(200)
       expect(json["data"].length).to eq(10)
+    end
+  end
+
+  describe "GET /todo" do
+    it "sends one todo" do
+      get todos_path, params: {id: todo.to_param}
+      expect(response).to have_http_status(200)
+      expect(json["data"].length).to eq(1)
     end
   end
 end
