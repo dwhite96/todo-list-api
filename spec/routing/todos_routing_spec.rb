d49fe88b6cb2cf124847f324158ccb3525137d48
David White <whited75@gmail.com>
2016-06-24 09:48:23 -0600

    Remove #new and #edit routing tests

--- spec/routing/todos_routing_spec.rb
+++ spec/routing/todos_routing_spec.rb
@@ -7,16 +7,8 @@ RSpec.describe TodosController, type: :routing do
       expect(:get => "/todos").to route_to("todos#index")
     end
 
-    it "routes to #new" do
-      expect(:get => "/todos/new").to route_to("todos#new")
-    end
-
     it "routes to #show" do
       expect(:get => "/todos/1").to route_to("todos#show", :id => "1")
-    end
-
-    it "routes to #edit" do
-      expect(:get => "/todos/1/edit").to route_to("todos#edit", :id => "1")
     end
 
     it "routes to #create" do
@@ -34,6 +26,5 @@ RSpec.describe TodosController, type: :routing do
     it "routes to #destroy" do
       expect(:delete => "/todos/1").to route_to("todos#destroy", :id => "1")
     end
-
   end
 end
