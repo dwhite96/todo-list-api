cc6340793eade86ed6c4ec9185c21f32eacc8944
David White <whited75@gmail.com>
2016-07-05 17:59:22 -0600

    Change http verb method arguments to uri format

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe "Todos", type: :request do
   describe "GET /todos" do
     it "responds with a list of todos" do
       todo_list
-      get todos_path
+      get "/todos"
       expect(response).to have_http_status(200)
       expect(json["data"].length).to eq(5)
     end
@@ -22,28 +22,28 @@ RSpec.describe "Todos", type: :request do
 
   describe "GET /todos/:id" do
     it "responds with one todo" do
-      get todos_path, params: { id: todo.to_param }
+      get "/todos/#{todo.id}"
       expect(response).to have_http_status(200)
-      expect(json["data"].length).to eq(1)
+      expect(json.length).to eq(1)
     end
   end
 
   describe "POST /todos" do
     context "with valid data" do
       it "responds with http status code 201" do
-        post todos_path, params: { todo: valid_attributes }
+        post "/todos", params: { todo: valid_attributes }
         expect(response).to have_http_status(201)
       end
     end
 
     context "with invalid data" do
       it "responds with http status code 422" do
-        post todos_path, params: { todo: invalid_attributes }
+        post "/todos", params: { todo: invalid_attributes }
         expect(response).to have_http_status(422)
       end
 
       it "responds with a 'can't be blank' error message" do
-        post todos_path, params: { todo: invalid_attributes }
+        post "/todos", params: { todo: invalid_attributes }
         expect(json["title"]).to eq(["can't be blank"])
       end
     end
