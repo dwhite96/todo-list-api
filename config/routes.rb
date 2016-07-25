930a88f340b16f88225d3d522512035efec0a126
David White <whited75@gmail.com>
2016-07-25 17:31:58 -0600

    Modify app to accept cross origin requests in development

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  match '*all', to: 'application#preflight', via: [:options]
   resources :todos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
